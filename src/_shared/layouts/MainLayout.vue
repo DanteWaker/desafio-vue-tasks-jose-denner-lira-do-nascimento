f1c2b05e722ade00f1ac533597d50a285c75785d
Denner <denner.nascimento@protonmail.com>
2025-11-21 17:21:48 -0300

    style: clean up whitespace and formatting in MainLayout.vue

--- src/_shared/layouts/MainLayout.vue
+++ src/_shared/layouts/MainLayout.vue
@@ -6,17 +6,11 @@ import Logo from "../components/logo/Logo.vue";
 
 <template>
   <div class="flex h-screen bg-gray-50">
-    
-
     <div class="flex-1 flex flex-col">
       <header class="h-16 bg-white border-b border-gray-200 flex items-center justify-between px-6 sticky top-0 z-20">
         <Logo />
-
         <Input />
-
         <Button />
-
-       
       </header>
 
       <main class="flex-1 overflow-auto p-6">
