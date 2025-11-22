4657733a83d30aa877f74c4522df6cdabad6b5e1
Denner <denner.nascimento@protonmail.com>
2025-11-21 21:47:18 -0300

    refactor: clean up router configuration and remove unused routes

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -2,31 +2,18 @@ import { createRouter, createWebHistory } from "vue-router";
 import TasksView from "@/modules/tasks/views/TasksView.vue";
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: [
-		{
-			path: "/",
-			redirect: "/tasks",
-		},
-		{
-			path: "/tasks",
-			name: "tasks",
-			component: TasksView,
-		},
-		{
-			path: "/dashboard",
-			name: "dashboard",
-			component: TasksView, // Placeholder
-		},
-		// {
-		//   path: '/about',
-		//   name: 'about',
-		//   // route level code-splitting
-		//   // this generates a separate chunk (About.[hash].js) for this route
-		//   // which is lazy-loaded when the route is visited.
-		//   component: () => import('../views/AboutView.vue'),
-		// },
-	],
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      redirect: "/tasks",
+    },
+    {
+      path: "/tasks",
+      name: "tasks",
+      component: TasksView,
+    },
+  ],
 });
 
 export default router;
