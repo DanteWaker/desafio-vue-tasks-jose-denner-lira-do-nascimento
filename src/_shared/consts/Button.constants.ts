410ce28f897c3efb56e82bc7c463406c3ddfb076
Denner <denner.nascimento@protonmail.com>
2025-11-21 23:09:54 -0300

    feat: add VARIANT_CLASSES constant for button styling

---
+++ src/_shared/consts/Button.constants.ts
@@ -0,0 +1,8 @@
+export const VARIANT_CLASSES = {
+  primary:
+    "text-white bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 border-transparent",
+  secondary:
+    "text-gray-700 bg-white border-gray-300 hover:bg-gray-50 focus:ring-gray-400",
+  danger:
+    "text-white bg-red-600 hover:bg-red-700 focus:ring-red-500 border-transparent",
+} as const;
