1019e19959fc03b5bc400a106ac36a0b904e2917
Denner <denner.nascimento@protonmail.com>
2025-11-21 21:44:52 -0300

    feat: refactor TasksView to utilize useTasksView for improved state management

--- src/modules/tasks/view-models/useTasksView.ts
+++ src/modules/tasks/view-models/useTasksView.ts
@@ -1,3 +1,17 @@
+import { computed } from "vue";
+import { storeToRefs } from "pinia";
+import { useGlobalStore } from "@/_shared/stores/GlobalStore";
+
 export function useTasksView() {
-    // Implementation of the useTasksView function
-    }
+  const globalStore = useGlobalStore();
+  const { tasks } = storeToRefs(globalStore);
+  const { removeTask } = globalStore;
+
+  const hasTasks = computed(() => tasks.value.length > 0);
+
+  return {
+    tasks,
+    hasTasks,
+    removeTask,
+  };
+}
--- src/modules/tasks/views/TasksView.vue
+++ src/modules/tasks/views/TasksView.vue
@@ -1,18 +1,14 @@
 <script setup lang="ts">
-import { storeToRefs } from "pinia";
-import { useGlobalStore } from "../../../_shared/stores/GlobalStore";
-
 import TaskEmpty from "./components/task-empty/TaskEmpty.vue";
 import TaskList from "./components/task-list/TaskList.vue";
+import { useTasksView } from "../view-models/useTasksView";
 
-const globalStore = useGlobalStore();
-const { tasks } = storeToRefs(globalStore);
-const { removeTask } = globalStore;
+const { tasks, hasTasks, removeTask } = useTasksView();
 </script>
 
 <template>
   <div class="space-y-6">
-    <TaskEmpty v-if="tasks.length === 0" />
+    <TaskEmpty v-if="!hasTasks" />
     <TaskList v-else :tasks="tasks" @remove-task="removeTask" />
   </div>
 </template>
